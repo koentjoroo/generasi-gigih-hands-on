b93d428b175d4bf30c129dac6bf99799d1c4fb04
Novan <enerka4ig@gmail.com>
2021-07-15 11:55:20 +0700

    Finished hands on `0.2.1`

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <input type="text" name="search" placeholder="Search..." />
+      <input type="submit" value="Search" />
+      <img
+        src="https://media.giphy.com/media/Vh8pbGX3SGRwFDh3V0/source.gif"
+        alt="Source GIF"
+      />
+    </div>
+  );
+}
