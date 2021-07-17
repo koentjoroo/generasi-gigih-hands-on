b71024579cd137dfb397ca4c3a92b8dce80ac2b3
Novan <enerka4ig@gmail.com>
2021-07-17 11:58:49 +0700

    Done `0.2.3`: List rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { gif } from "./data/sample";
+import { gifs } from "./data/sample";
 import Search from "./components/Search";
 import Gif from "./components/Gif";
 import "./App.css";
 
 function App() {
-  const { url, title } = gif;
   return (
     <div className="App">
       <Search />
-      <Gif url={url} title={title} />
+      {gifs.map(({ id, title, webp, rating }) => (
+        rating === 'g' && <Gif url={webp} title={title} key={id} />
+      ))}
     </div>
   );
 }
